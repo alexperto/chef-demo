fe32e7a856dad4af644e6e6f481ee1adb2c20f8a
Alex Cabrera <alexperto@gmail.com>
2014-05-12 13:04:26 -0700

    Setup apache recipe

--- cookbooks/apache-tutorial-1/recipes/default.rb
+++ cookbooks/apache-tutorial-1/recipes/default.rb
@@ -6,3 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+#
+package 'apache2' do
+  action :install
+end
+
+service 'apache2' do
+  action [ :enable, :start ]
+end
+
+cookbook_file '/var/www/index.html' do
+  source 'index.html'
+  mode '0644'
+end
